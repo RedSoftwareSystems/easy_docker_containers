8f446d9fcdc721c680e2effc81804fa988d7cbaa
Jacob Fogg <jacobfogg@gmail.com>
2020-02-19 13:32:41 -0700

    Added active container count to topbar

--- src/dockerMenu.js
+++ src/dockerMenu.js
@@ -2,11 +2,14 @@
 
 const St = imports.gi.St;
 const Gio = imports.gi.Gio;
+const GLib = imports.gi.GLib;
 const panelMenu = imports.ui.panelMenu;
 const { arrowIcon, PopupMenuItem } = imports.ui.popupMenu;
 const extensionUtils = imports.misc.extensionUtils;
+const Lang = imports.lang;
 const Me = extensionUtils.getCurrentExtension();
 const Docker = Me.imports.src.docker;
+const Mainloop = imports.mainloop;
 const { DockerSubMenu } = Me.imports.src.dockerSubMenuMenuItem;
 const GObject = imports.gi.GObject;
 
@@ -19,13 +22,19 @@ var DockerMenu = GObject.registerClass(
       const hbox = new St.BoxLayout({ style_class: "panel-status-menu-box" });
       const gicon = Gio.icon_new_for_string(Me.path + "/docker.svg");
       const dockerIcon = new St.Icon({ gicon: gicon, icon_size: "24" });
+      
+      this.buttonText = new St.Label({
+        text: _("Loading...")
+      });
 
       hbox.add_child(dockerIcon);
       hbox.add_child(arrowIcon(St.Side.BOTTOM));
+      hbox.add_child(this.buttonText);
       this.add_child(hbox);
       this.connect("button_press_event", this._refreshMenu.bind(this));
 
       this._renderMenu();
+      this._refreshCount();
     }
 
     // Refresh  the menu everytime the user click on it
@@ -35,6 +44,29 @@ var DockerMenu = GObject.registerClass(
         this.menu.removeAll();
         this._renderMenu();
       }
+    }
+
+    _refreshCount() {
+      let dockerCount = 0;
+      try{
+        let containers = Docker.getContainers();
+        if (containers.length > 0) {
+          containers.forEach(container => {
+            if(container.status.indexOf("Up") > -1){
+              dockerCount++;
+            }
+          });
+        }
+      } catch (err) {}
+      
+      this.buttonText.set_text(dockerCount+'');
+
+      if (this._timeout) {
+        Mainloop.source_remove(this._timeout);
+        this._timeout = null;
+      }
+
+      this._timeout = Mainloop.timeout_add_seconds(2, Lang.bind(this, this._refreshCount));
     }
 
     // Show docker menu icon only if installed and append docker containers
@@ -76,3 +108,4 @@ var DockerMenu = GObject.registerClass(
     }
   }
 );
+`
